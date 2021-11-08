2684d32ce84dd9a93c3aecf3ee828e792e6a49f2
Hamish <hamish.gamble@flinders.edu.au>
2021-11-08 16:41:17 +1030

    Uses inbuilt location_quotient function for calculating industry comparative advantage

--- data-raw/industry_comparative_advantage.R
+++ data-raw/industry_comparative_advantage.R
@@ -1,7 +1,14 @@
 ## code to prepare `industry_comparative_advantage` dataset goes here
-library(reticulate)
-library(tidyverse)
+library(dplyr)
+library(aurininnovation)
 
+
+industry_comparative_advantage <- bind_rows(
+  location_quotient(sa2_indp2_2016,
+                    options = list(x = "sa2_name", y = "industry_2", total = "region_employment", value = "employment")) %>% mutate(year = 2016),
+  location_quotient(sa2_indp2_2011,
+                    options = list(x = "sa2_name", y = "industry_2", total = "region_employment", value = "employment")) %>% mutate(year = 2011)
+)
 
 
 
