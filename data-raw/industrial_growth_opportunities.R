0e281d4d09fa38fd974fad4a878ddeb54d4a2d25
Hamish Gamble <hamish.gamble@gmail.com>
2022-02-04 17:21:07 +1030

    Create industrial_growth_opportunities dataset

--- data-raw/industrial_growth_opportunities.R
+++ data-raw/industrial_growth_opportunities.R
@@ -20,11 +20,26 @@ states <- c("NSW",
             "NT",
             "ACT")
 
-state_ec <- get_data(states, year = 2016)
+state_ec <- get_data(states, years = 2016) %>%
+  mutate(location_code = clean_state(location_code, to = "state_name"))
 
 #1 - Keep only ICA >=1
 #2 - RCA product < 0
 #3 - Export product > 0
 #4 - COG > 0
 
-#usethis::use_data(industrial_growth_opportunities, overwrite = TRUE)
+industrial_growth_opportunities <- state_ec %>%
+  filter(export_value > 0,
+         cog > 0,
+         rca < 1) %>%
+  left_join(anzsic_hs, by = "hs_product_code") %>%
+  left_join(ica_with_state, by = c("location_code" = "state_name_2016",
+                                   "anzsic_subdivision" = "industry",
+                                   "year")) %>%
+  filter(comparative_advantage >= 1) %>%
+  add_product_names(digits = "4") %>%
+  arrange(sa2_name, sa2_code_2016)
+
+
+
+usethis::use_data(industrial_growth_opportunities, overwrite = TRUE)
