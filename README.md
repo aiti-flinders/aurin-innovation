2af1e54790dae61026f95b15c7ae52e52c2539f7
Hamish <hamish.gamble@flinders.edu.au>
2022-02-07 11:21:14 +1030

    Uses purrr to create indicator for multiple years.

---
+++ README.md
@@ -0,0 +1,16 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# aurininnovation
+
+This package contains the data analysis component of the AURIN High
+Impact Project “Quantifying Regional Innovation”.
+
+Datasets required for this project include:
+
+1.  Industrial comparative advantage at the SA2 level for 2011, 2016,
+    and 2021 pending release of ABS 2021 census.
+2.  Industrial growth opportunities at the SA2 level for 2011, 2016, and
+    2021 pending release of ABS 2021 census.
+3.  Knowledge capability at the SA2 level.
+4.  Innovation at the SA2 level.
--- data-raw/industrial_growth_opportunities.R
+++ data-raw/industrial_growth_opportunities.R
@@ -2,45 +2,57 @@
 #Industrial growth opportunities links state economic complexity data with SA2 industry comparative advantage data
 
 library(ecomplexity)
+library(purrr)
 library(strayr)
 library(dplyr)
 
 devtools::load_all()
 
-ica_with_state <- industry_comparative_advantage %>%
-  left_join(read_absmap(area = "sa2", year = "2016")) %>%
-  select(sa2_name, sa2_code_2016, industry, comparative_advantage, year, state_name_2016)
+iga <- function(year) {
 
-states <- c("NSW",
-            "VIC",
-            "QLD",
-            "SA",
-            "WA",
-            "TAS",
-            "NT",
-            "ACT")
+  states <- c("NSW",
+              "VIC",
+              "QLD",
+              "SA",
+              "WA",
+              "TAS",
+              "NT",
+              "ACT")
 
-state_ec <- get_data(states, years = 2016) %>%
-  mutate(location_code = clean_state(location_code, to = "state_name"))
+  ica_with_state <- industry_comparative_advantage %>%
+    left_join(read_absmap(area = "sa2", year = {{year}}, remove_year_suffix = TRUE)) %>%
+    mutate(year = as.numeric({{year}})) %>%
+    select(sa2_name, sa2_code, industry, comparative_advantage, year, state_name)
 
-#1 - Keep only ICA >=1
-#2 - RCA product < 0
-#3 - Export product > 0
-#4 - COG > 0
+  state_ec <- get_data(states, years = {{year}}) %>%
+    mutate(location_code = clean_state(location_code, to = "state_name"))
 
-industrial_growth_opportunities <- state_ec %>%
-  filter(export_value > 0,
-         cog > 0,
-         rca < 1) %>%
-  left_join(anzsic_hs, by = "hs_product_code") %>%
-  left_join(ica_with_state, by = c("location_code" = "state_name_2016",
-                                   "anzsic_subdivision" = "industry",
-                                   "year")) %>%
-  filter(comparative_advantage >= 1) %>%
-  add_product_names(digits = "4") %>%
-  arrange(sa2_name, sa2_code_2016) %>%
-  select(sa2_name, sa2_code_2016, location_code, anzsic_subdivision, hs_product_name_short_en, export_value, pci, cog, rca)
+  #1 - Keep only ICA >=1
+  #2 - RCA product < 0
+  #3 - Export product > 0
+  #4 - COG > 0
 
+  industrial_growth_opportunities <- state_ec %>%
+    filter(export_value > 0,
+           cog > 0,
+           rca < 1) %>%
+    left_join(anzsic_hs, by = "hs_product_code") %>%
+    left_join(ica_with_state, by = c("location_code" = "state_name",
+                                     "anzsic_subdivision" = "industry",
+                                     "year")) %>%
+    filter(comparative_advantage >= 1) %>%
+    add_product_names(digits = "4") %>%
+    arrange(sa2_name, sa2_code) %>%
+    select(year, sa2_name, sa2_code, location_code, anzsic_subdivision, hs_product_name_short_en, export_value, pci, cog, rca)
+
+  return(industrial_growth_opportunities)
+
+
+
+
+}
+
+industrial_growth_opportunities <- map_df(.x = c("2011", "2016"), .f = ~iga(.x))
 
 
 usethis::use_data(industrial_growth_opportunities, overwrite = TRUE, compress = "xz")
