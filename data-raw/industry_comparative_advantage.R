e1227cfadbe56b162170b945611c3b53fd1c8d90
Hamish <hamish.gamble@flinders.edu.au>
2022-02-07 13:28:46 +1030

    No longer use year suffixes for geometry columns

--- data-raw/industry_comparative_advantage.R
+++ data-raw/industry_comparative_advantage.R
@@ -11,18 +11,18 @@ industry_comparative_advantage <- bind_rows(
                                    total = "region_employment",
                                    value = "employment")) %>%
     mutate(year = 2016) %>%
-    left_join(read_absmap("sa22016"), by = c("sa2_name" = "sa2_name_2016")) %>%
-    select(sa2_name, sa2_code_2016, industry, comparative_advantage = rca, year),
+    left_join(read_absmap(name = "sa22016", remove_year_suffix = TRUE)) %>%
+    select(sa2_name, sa2_code, industry, comparative_advantage = rca, year),
   location_quotient(sa2_indp2_2011,
                     options = list(x = "sa2_name",
                                    y = "industry_2",
                                    total = "region_employment",
                                    value = "employment")) %>%
     mutate(year = 2011) %>%
-    left_join(read_absmap("sa22011"), by = c("sa2_name" = "sa2_name_2011")) %>%
-    select(sa2_name, sa2_code_2011, industry, comparative_advantage = rca, year)
+    left_join(read_absmap("sa22011", remove_year_suffix = TRUE)) %>%
+    select(sa2_name, sa2_code, industry, comparative_advantage = rca, year)
 )
 
 
 
-usethis::use_data(industry_comparative_advantage, overwrite = TRUE)
+usethis::use_data(industry_comparative_advantage, overwrite = TRUE, compress = "xz")
