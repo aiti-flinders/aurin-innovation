d8c3ed71971a8ee33f5be96698f743ecd84a1f01
Hamish Gamble <hamish.gamble@gmail.com>
2022-05-04 15:28:46 +0930

    Don't replace NAs here

--- R/knowledge_capability.R
+++ R/knowledge_capability.R
@@ -11,11 +11,7 @@ create_knowledge_capability <- function(year, geography) {
     dplyr::left_join(
       y = purrr::map_dfr(.x = year, .f = ~create_plants(.x, {{geography}})),
       by = c({{geography}}, "year")
-    ) %>%
-    tidyr::replace_na(list(patents = 0,
-                           designs = 0,
-                           trademarks = 0,
-                           plants = 0))
+    )
 
   return(knowledge_capability)
 
