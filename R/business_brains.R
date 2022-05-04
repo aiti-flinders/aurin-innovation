1772119f3ffc7d2caea4905bd9cfacd7101a2c29
Hamish Gamble <hamish.gamble@gmail.com>
2022-05-04 15:26:11 +0930

    Style

--- R/business_brains.R
+++ R/business_brains.R
@@ -1,7 +1,7 @@
 create_business_brains <- function(year, geography, adjust) {
 
-  dplyr::full_join(create_kibs(year, geography, adjust), create_skill(year, geography, adjust)) %>%
-    dplyr::full_join(sa3_innovation %>% dplyr::filter(year == 2015) %>% dplyr::select(sa3_name, berd)) %>%
+  dplyr::full_join(create_kibs(year, geography, adjust),
+                   create_skill(year, geography, adjust)) %>%
     dplyr::full_join(create_owner_managers(year, geography))
 
 
