e1c52f3b37d2a77795ec888ff8335f8de158a1d2
Hamish Gamble <hamish.gamble@gmail.com>
2022-05-04 15:31:15 +0930

    Document datasets

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Intellectual Property Government Data
+#'
+#' A dataset containing the number of patents, trademarks, designs, and plant
+#' breeder's rights occuring in Australian SA2s between 2011 and 2017.
+#'
+#' @format A data frame with 8181 rows and 6 variables:
+#' \describe{
+#'   \item{sa2_name}{name of the SA2}
+#'   \item{year}{year}
+#'   \item{patents}{number of non-petty patents which were not refused or revoked provided to entities}
+#'   \item{designs}{number of designs currently in force provided to entitites}
+#'   \item{trademarks}{number of trademarks not refused or rejected provided to entities}
+#'   \item{plant_breeder_rights}{number of plant breeder rights granted, accepted, and recieved by entities}
+#' }
+#' @source \url{https://www.ipaustralia.gov.au/about-us/research-and-data/ip-government-open-data}
