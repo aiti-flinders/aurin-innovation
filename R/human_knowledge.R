5cde0f279b0e87964007a7bcd8300ede1656430d
Hamish Gamble <hamish.gamble@gmail.com>
2022-05-04 10:34:51 +0930

    Allows creation of stem and education data for 2011

--- R/human_knowledge.R
+++ R/human_knowledge.R
@@ -5,14 +5,14 @@ create_graduates <- function(year = 2016, geography = "sa3_name") {
     geog <- strayr::read_absmap("sa22016", remove_year_suffix = TRUE) %>%
       sf::st_drop_geometry()
 
-    data <- sa2_heap1_2016
+    data <- sa2_qallp1_2016
 
   } else {
 
     geog <- strayr::read_absmap("sa22011", remove_year_suffix = TRUE) %>%
       sf::st_drop_geometry()
 
-    data <- sa2_heap1_2011
+    data <- sa2_qallp1_2011
   }
 
 
@@ -22,18 +22,15 @@ create_graduates <- function(year = 2016, geography = "sa3_name") {
     dplyr::summarise(employment = sum(employment), .groups = "drop") %>%
     tidyr::pivot_wider(names_from = education_level,
                        values_from = employment) %>%
-    dplyr::mutate(university = `Bachelor Degree Level` + `Postgraduate Degree Level`,
-                  tafe = `Certificate III & IV Level` + `Certificate I & II Level` + `Advanced Diploma and Diploma Level`,
-                  school = `Secondary Education - Years 10 and above`,
-                  no_school = `Secondary Education - Years 9 and below`) %>%
-    dplyr::select(geography,
-                  university,
-                  tafe,
-                  school,
-                  no_school) %>%
-    dplyr::mutate(total = university + tafe + school + no_school,
-                  dplyr::across(c(university, tafe, school, no_school), ~.x / total)) %>%
-    dplyr::select(-total)
+    janitor::clean_names() %>%
+    dplyr::mutate(dip = advanced_diploma_and_diploma_level,
+                  cert = certificate_level,
+                  grad = bachelor_degree_level + graduate_diploma_and_graduate_certificate_level,
+                  postgrad = postgraduate_degree_level,
+                  rank = (4 * postgrad + 3 * grad + 2 * dip + 1 * cert) / (postgrad + grad + dip + cert)) %>%
+    dplyr::select(.data[[geography]],
+                  rank)
+
 
 
 }
@@ -52,7 +49,7 @@ create_stem <- function(year, geography) {
     geog <- strayr::read_absmap("sa22011", remove_year_suffix = TRUE) %>%
       sf::st_drop_geometry()
 
-    data <- sa2_qalfp1_2011
+    data <- sa2_qalfp2_2011
   }
 
 
@@ -65,8 +62,9 @@ create_stem <- function(year, geography) {
                     .data[[geography]]) %>%
     dplyr::summarise(stem_employees = sum(employment), .groups = "drop")  %>%
     tidyr::pivot_wider(names_from = stem, values_from = stem_employees, names_prefix = "stem") %>%
-    dplyr::mutate(stem_share = stemTRUE/(stemFALSE + stemTRUE)) %>%
-    dplyr::select(geography, stem_share)
+    dplyr::mutate(stem = stemTRUE/(stemFALSE + stemTRUE),
+                  year = {{year}}) %>%
+    dplyr::select(geography, stem, year)
 
 }
 
