c0a6c926df03ddf4dee492cd6e26116b5bbd9c61
Hamish Gamble <hamish.gamble@gmail.com>
2022-05-04 15:27:08 +0930

    Calculate infrastructure for 2011 and 2016

--- R/infrastructure.R
+++ R/infrastructure.R
@@ -1,11 +1,21 @@
 create_infrastructure <- function(year, geography = "sa3_name") {
 
-  sa3_innovation %>%
-    dplyr::filter(year == {{year}}) %>%
-    dplyr::select({{geography}},
-                  unis,
-                  tafes,
-                  ris)
+  if (year == 2011) {
+
+    geog <- strayr::read_absmap("sa22011", remove_year_suffix = TRUE)
+    data <- education_location
+
+  } else if (year == 2016) {
+
+    geog <- strayr::read_absmap("sa22016", remove_year_suffix = TRUE)
+    data <- education_location
+  }
+
+  data %>%
+    dplyr::left_join(geog) %>%
+    dplyr::group_by(.data[[geography]]) %>%
+    dplyr::summarise(dplyr::across(c(unis, tafes), ~sum(.x, na.rm = TRUE))) %>%
+    dplyr::ungroup()
 
 
 
