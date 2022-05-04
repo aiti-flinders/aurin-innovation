8a2ac596699c9274f547d6d3a94b7ff496f07460
Hamish Gamble <hamish.gamble@gmail.com>
2022-05-04 10:38:10 +0930

    Adds patent citations and includes all patent applications from valid SA2s

--- R/patents.R
+++ R/patents.R
@@ -26,28 +26,56 @@ create_patents <- function(year, geography = NULL) {
     patents_info <- readr::read_csv("data-raw/ipgod101.csv")
   }
 
-  if (year < 2016) {
+  if (!file.exists("data-raw/ipgod110.csv")) {
+
+    download.file("https://data.gov.au/data/dataset/a4210de2-9cbb-4d43-848d-46138fefd271/resource/12d28b0f-b16e-487d-b6ca-b44ea7b97d8c/download/ipgod110.csv",
+                  destfile = "data-raw/ipgod110.csv")
+
+    patents_citations <- readr::read_csv("data-raw/ipgod110.csv")
+
+  } else {
+
+    patents_citations <- readr::read_csv("data-raw/ipgod110.csv")
+
+  }
+
+  if (year == 2011) {
+
     geog <- strayr::read_absmap("sa22011", remove_year_suffix = TRUE) %>%
       sf::st_drop_geometry()
-    }
-  else {
+
+  }
+
+  else if (year == 2016) {
+
     geog <- strayr::read_absmap("sa22016", remove_year_suffix = TRUE) %>%
       sf::st_drop_geometry()
+
+  } else {
+
+      stop("Year must be one of 2011 or 2016.")
     }
 
   patents %>%
-    dplyr::mutate(type = "patents",
-                  year = {{year}}) %>%
     dplyr::left_join(patents_info) %>%
+    dplyr::left_join(patents_citations) %>%
+    dplyr::mutate(ipc = stringr::str_sub(primary_ipc_mark_value, 0, 1)) %>%
     dplyr::filter(application_year == {{year}},
-                  australian == TRUE,
-                  !patent_status_type %in% c("LAPSED", "REFUSED", "REVOKED", "WITHDRAWN"),
-                  patent_type != "Provisional") %>%
-    dplyr::left_join(geog) %>%
-    dplyr::group_by(.data[[geography]], type, year) %>%
-    dplyr::tally() %>%
+                  !is.na(sa2_name)) %>%
+    dplyr::group_by(australian_appl_no) %>%
+    dplyr::mutate(backwards_citations = dplyr::n()) %>%
     dplyr::ungroup() %>%
-    tidyr::pivot_wider(names_from = type,
-                       values_from = n) %>%
-    dplyr::filter(!is.na(.data[[geography]]))
+    dplyr::left_join(geog) %>%
+    dplyr::group_by(.data[[geography]], application_year) %>%
+    dplyr::summarise(patents = dplyr::n(),
+                     backwards_citations = mean(backwards_citations)) %>%
+    dplyr::ungroup() %>%
+    dplyr::rename(year = application_year)
+    # tidyr::pivot_wider(names_from = ipc,
+    #                    values_from = patents) %>%
+    # dplyr::filter(!is.na(.data[[geography]])) %>%
+    # tidyr::replace_na(list(A = 0, B = 0, C = 0, D = 0, E = 0, `F` = 0, G = 0, H = 0)) %>%
+    # dplyr::rename(year = application_year) %>%
+    # dplyr::group_by(.data[[geography]], year) %>%
+    # dplyr::summarise(dplyr::across(tidyselect:::where(is.integer), ~sum(.x)), .groups = "drop")
 }
