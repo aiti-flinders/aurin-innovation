cec79862a876daa3256f92c06c95abb79db4fc02
Hamish Gamble <hamish.gamble@gmail.com>
2022-05-04 15:26:41 +0930

    Allow ica to be calculated at different geographies

--- R/ica.R
+++ R/ica.R
@@ -2,13 +2,13 @@
 #'
 #' @param years Which year to calculate the industrial comparative advantage. The default, "all", will calculate the
 #' industrial comparative advantage for all available years. Available years are 2011 and 2016.
-#' @param ... Additional options passed to the calculation of the location quotient. See ?location_quotient for more information.
+#' @param ... Additional options passed to the calculation of the location quotient. See `location_quotient` for more information.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-ica <- function(years = "all", ...) {
+ica <- function(years = "all", geography = "sa2_name", ...) {
 
   years <- as.numeric(years)
 
@@ -46,6 +46,17 @@ ica <- function(years = "all", ...) {
       dplyr::mutate(sa2_employment = sum(employment)) %>%
       dplyr::ungroup()
   }
+
+  if (is.null(ellipse_arg$value_var)) {
+
+    value_var <- "employment"
+    ellipse_arg$value_var = "employment"
+  }
+  pow <- pow %>%
+    dplyr::left_join(strayr::read_absmap("sa22016", remove_year_suffix = TRUE)) %>%
+    dplyr::group_by(.data[[geography]], industry) %>%
+    dplyr::summarise(dplyr::across(.data[[ellipse_arg$value_var]], sum)) %>%
+    dplyr::ungroup()
 
 
 
