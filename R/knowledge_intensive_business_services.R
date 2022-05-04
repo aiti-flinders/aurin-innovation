c9e09185d08a1de966e8d277cfc706e6af150799
Hamish Gamble <hamish.gamble@gmail.com>
2022-05-04 10:41:40 +0930

    Allow creation of kibs dataset for 2011

--- R/knowledge_intensive_business_services.R
+++ R/knowledge_intensive_business_services.R
@@ -1,44 +1,44 @@
 create_kibs <- function(year = NULL, geography = NULL, adjust = TRUE) {
 
-  if (adjust) {
-
-    data <- sa2_indp4_2016 %>%
-      adjust_nfd(nfd_col = "anzsic_class",
-                 nfd_level = "anzsic_class",
-                 anz = "anzsic")
-
-  } else {
-
-    data <- sa2_indp4_2016
-
-  }
 
   if (year == 2016) {
 
     geog <- strayr::read_absmap("sa22016", remove_year_suffix = TRUE) %>%
       sf::st_drop_geometry()
 
-  } else {
+    data <- sa2_indp4_2016
+
+
+  } else if (year == 2011) {
       geog <- strayr::read_absmap("sa22011", remove_year_suffix = TRUE) %>%
         sf::st_drop_geometry()
-    }
+
+      data <- sa2_indp4_2011
+  }
+
+  if (adjust) {
+
+    data <- data %>%
+      adjust_nfd(nfd_col = "anzsic_class",
+                 nfd_level = "anzsic_class",
+                 anz = "anzsic") %>%
+      dplyr::select(sa2_name, anzsic_class, employment = employment_adj)
+
+  } else {
+
+    data <- data
+
+  }
 
   employment_kibs <- data %>%
-    dplyr::group_by(kibs = anzsic_class %in% kibs(),
-                    sa2_name) %>%
-    dplyr::mutate(employment = sum(employment)) %>%
-    dplyr::ungroup() %>%
-    dplyr::distinct(sa2_name, employment, kibs) %>%
-    tidyr::pivot_wider(names_from = kibs,
-                       names_prefix = "kibs",
-                       values_from = employment)  %>%
     dplyr::left_join(geog) %>%
-    dplyr::group_by(.data[[geography]]) %>%
-    dplyr::summarise(dplyr::across(c(kibsFALSE, kibsTRUE), ~sum(.x)), .groups = "drop") %>%
-    dplyr::mutate(total_employment = kibsTRUE + kibsFALSE) %>%
-    dplyr::rename(non_kibs = kibsFALSE,
-                  kibs = kibsTRUE) %>%
-    dplyr::select(-total_employment)
+    dplyr::group_by(kibs = anzsic_class %in% kibs(),
+                    .data[[geography]]) %>%
+    dplyr::summarise(kibs_employment = sum(employment), .groups = "drop") %>%
+    tidyr::pivot_wider(names_from = kibs, names_prefix = "kibs", values_from = kibs_employment) %>%
+    dplyr::mutate(kibs = kibsTRUE / (kibsFALSE + kibsTRUE),
+                  year = {{year}}) %>%
+    dplyr::select(geography, kibs, year)
 
 
 
