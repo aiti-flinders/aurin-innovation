2283925dd39422ae0aaf0403c9c7136e9e66da37
Hamish <hamish.gamble@flinders.edu.au>
2022-05-11 13:55:58 +0930

    Add regional innovation function

--- R/knowledge_capability.R
+++ R/knowledge_capability.R
@@ -7,12 +7,6 @@ create_knowledge_capability <- function(year, geography) {
     dplyr::full_join(create_trademarks(year, geography), by = join_by) %>%
     dplyr::full_join(create_plants(year, geography), by = join_by)
 
-  knowledge_capability %>%
-    tidyr::replace_na(list(patents = 0,
-                           backwards_citations = 0,
-                           designs = 0,
-                           trademarks = 0,
-                           plants = 0))
-
+  knowledge_capability
 
 }
