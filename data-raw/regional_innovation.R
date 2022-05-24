72601639f5e2d3eecc795bfb33996793289f6fcf
Hamish Gamble <gamb0043@flinders.edu.au>
2022-05-25 09:03:53 +0930

    Add sa2_codes to data

--- data-raw/regional_innovation.R
+++ data-raw/regional_innovation.R
@@ -1,11 +1,13 @@
 ## code to prepare `regional_innovation` dataset goes here
 library(aurininnovation)
 library(purrr)
+library(sf)
 library(scales)
 library(dplyr)
 
 regional_innovation <- map_dfr(.x = c(2011, 2016),
                                .f = ~create_regional_innovation(year = .x, geography = "sa2", adjust = FALSE)) %>%
-  mutate(across(c(innovation, human_knowledge, patent_output), ~rescale(.x, to = c(0, 100))))
+  mutate(across(c(innovation, human_knowledge, patent_output), ~rescale(.x, to = c(0, 100)))) %>%
+  aurininnovation:::add_sa2_codes()
 
 usethis::use_data(regional_innovation, overwrite = TRUE)
